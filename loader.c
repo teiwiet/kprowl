77d828b7fa57888adfb2a4c6aa480d7c29e1d636
teiwiet <teiwiet33@gmail.com>
2026-07-30 20:45:00 +0700

    feat(loader): load watchlist, report file-open events

--- loader.c
+++ loader.c
@@ -1,113 +1,131 @@
 #include <bpf/libbpf.h>
+#include <bpf/bpf.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <signal.h>
-#include <sys/wait.h>
 #include <errno.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 #include "kprowl.h"
 
 static volatile int running = 1;
-
-void handle_sigint(int sigint){
-    running = 0;
-}
+void handle_sigint(int sig) { running = 0; }
 
 static int on_event(void *ctx, void *data, size_t len)
 {
-	const struct event *e = data;
-	printf("pid=%-7u uid=%-5u comm=%-16s file=%s\n",
-	       e->pid, e->uid, e->comm, e->filename);
-	return 0;
+    const struct event *e = data;
+
+    switch (e->type) {
+    case EVENT_EXEC:
+        printf("[EXEC]  pid=%-6u uid=%-5u comm=%-16s exec=%s\n",
+               e->pid, e->uid, e->comm, e->exec.filename);
+        break;
+    case EVENT_FILE_OPEN:
+        printf("[FILE!] pid=%-6u uid=%-5u comm=%-16s SENSITIVE=%s\n",
+               e->pid, e->uid, e->comm, e->file.path);
+        break;
+    }
+    return 0;
 }
-int main(int argc,char **argv){
-    if(argc < 2){
-        printf("Usage : %s <program> \n",argv[0]);
+
+static void load_watchlist(struct bpf_map *wl)
+{
+    const char *home = getenv("HOME");
+    if (!home) home = "/root";
+
+    char ssh_rsa[512], ssh_ed[512], aws[512];
+    snprintf(ssh_rsa, sizeof(ssh_rsa), "%s/.ssh/id_rsa", home);
+    snprintf(ssh_ed, sizeof(ssh_ed), "%s/.ssh/id_ed25519", home);
+    snprintf(aws, sizeof(aws), "%s/.aws/credentials", home);
+
+    const char *paths[] = {
+        "/etc/shadow", "/etc/passwd", "/etc/sudoers",
+        ssh_rsa, ssh_ed, aws,
+        NULL
+    };
+
+    int fd = bpf_map__fd(wl);
+    for (int i = 0; paths[i]; i++) {
+        struct stat st;
+        if (stat(paths[i], &st) != 0)
+            continue;
+
+        struct file_key key = { .ino = st.st_ino };
+        struct wl_val val = {0};
+        strncpy(val.path, paths[i], sizeof(val.path) - 1);
+
+        if (bpf_map_update_elem(fd, &key, &val, BPF_ANY) == 0)
+            printf("[watchlist] %s (ino=%llu)\n",
+                   paths[i], (unsigned long long)st.st_ino);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        printf("Usage: %s <program> [args...]\n", argv[0]);
         return 1;
     }
-    struct bpf_object *obj = NULL;
-    struct bpf_link *link = NULL;
-    struct bpf_program *prog = NULL;
-    struct bpf_map *map = NULL;
-    struct bpf_map *tracked = NULL;
-    struct ring_buffer *rb = NULL;
-    int err = 0;
 
-    obj = bpf_object__open_file("kprowl.bpf.o",NULL);
-    if(!obj){
-        printf("Failed to open file");
-        return 1;
-    }
+    struct bpf_object *obj;
+    struct ring_buffer *rb;
 
-    if(bpf_object__load(obj)){
-        printf("Failed to load project into kernel try running with sudo\n");
-        return 1;
-    }
+    obj = bpf_object__open_file("kprowl.bpf.o", NULL);
+    if (!obj) { fprintf(stderr, "Failed to open BPF object\n"); return 1; }
 
-    prog = bpf_object__find_program_by_name(obj,"handle_execve");
-    if(!prog){
-        printf("Failed to find program name\n");
-        goto cleanup;
-    }
+    if (bpf_object__load(obj)) { fprintf(stderr, "Failed to load into kernel (try sudo)\n"); return 1; }
 
-    link = bpf_program__attach(prog);
-    if(!link){
-        printf("Failed to attach\n");
-        goto cleanup;
-    }
+    struct bpf_program *prog;
+    bpf_object__for_each_program(prog, obj)
+        if (!bpf_program__attach(prog))
+            fprintf(stderr, "warn: could not attach %s: %s\n",
+                    bpf_program__name(prog), strerror(errno));
 
-    map = bpf_object__find_map_by_name(obj,"events");
-    if(!map){
-        printf("Failed to find map\n");
-        goto cleanup;
-    }
-    
-    tracked = bpf_object__find_map_by_name(obj,"tracked");
-    if(!tracked){
-        printf("Failed to find map tracked\n");
-        goto cleanup;
-    }
+    struct bpf_map *events_map = bpf_object__find_map_by_name(obj, "events");
+    struct bpf_map *tracked = bpf_object__find_map_by_name(obj, "tracked");
+    struct bpf_map *watchlist = bpf_object__find_map_by_name(obj, "watchlist");
+    if (!events_map || !tracked || !watchlist) { fprintf(stderr, "Failed to find maps\n"); return 1; }
 
-    rb = ring_buffer__new(bpf_map__fd(map),on_event,NULL,NULL);
-    if(!rb){
-        printf("Failed to create ring buffer\n");
-        goto cleanup;
-    }
-    signal(SIGINT,handle_sigint);
+    load_watchlist(watchlist);
+
+    rb = ring_buffer__new(bpf_map__fd(events_map), on_event, NULL, NULL);
+    if (!rb) { fprintf(stderr, "Failed to create ring buffer\n"); return 1; }
+
+    signal(SIGINT, handle_sigint);
+
     pid_t child = fork();
-    
-    if(child < 0){
-        perror("fork");
-        goto cleanup;
-    }
-
-    if(child == 0){
+    if (child < 0) { perror("fork"); goto cleanup; }
+    if (child == 0) {
         raise(SIGSTOP);
-        execvp(argv[1],&argv[1]);
+        execvp(argv[1], &argv[1]);
         perror("execvp");
         _exit(127);
     }
+
+    int sstatus;
+    waitpid(child, &sstatus, WUNTRACED);
     __u32 pid = child;
     __u8 one = 1;
+    bpf_map_update_elem(bpf_map__fd(tracked), &pid, &one, BPF_ANY);
+    kill(child, SIGCONT);
 
-    bpf_map__update_elem(tracked,&pid,sizeof(pid),&one,sizeof(one),BPF_ANY);
-    kill(child,SIGCONT);
-    printf("[kprowl] tracing pid %d (%s)\n",child,argv[1]);
-    while(running){
-        int err = ring_buffer__poll(rb,100);
-        if(err == -EINTR) continue;
-        if(err < 0 ){
-            printf("Poll err : %d",err);
-            break;
-        }
+    printf("[kprowl] tracing pid %d (%s)\n", child, argv[1]);
+
+    while (running) {
+        int err = ring_buffer__poll(rb, 100);
+        if (err == -EINTR) continue;
+        if (err < 0) { fprintf(stderr, "poll error: %d\n", err); break; }
         int wstatus;
-        if(waitpid(child,&wstatus,WNOHANG)){
-            ring_buffer__poll(rb,100);
+        if (waitpid(child, &wstatus, WNOHANG) == child) {
+            ring_buffer__poll(rb, 100);
             break;
         }
     }
+
 cleanup:
-    bpf_link__destroy(link);
-    bpf_object__close(obj);
     ring_buffer__free(rb);
+    bpf_object__close(obj);
     return 0;
 }
