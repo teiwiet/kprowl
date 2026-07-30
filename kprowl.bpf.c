9fce1962675771d868ed471ef15a390a04a88ad6
teiwiet <teiwiet33@gmail.com>
2026-07-30 20:23:44 +0700

    feat(bpf): add security_file_open kprobe + watchlist map

--- kprowl.bpf.c
+++ kprowl.bpf.c
@@ -4,56 +4,76 @@
 #include <bpf/bpf_helpers.h>
 #include "kprowl.h"
 
-
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
-pid_t pid_filter = 0;
 
 struct {
-    __uint(type,BPF_MAP_TYPE_RINGBUF);
-    __uint(max_entries,256*1024);
-}events SEC(".maps");
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 256 * 1024);
+} events SEC(".maps");
 
 struct {
-    __uint(type,BPF_MAP_TYPE_HASH);
-    __uint(max_entries,8192);
-    __type(key,__u32);
-    __type(value,__u8);
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 8192);
+    __type(key, __u32);
+    __type(value, __u8);
 } tracked SEC(".maps");
 
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 1024);
+    __type(key, struct file_key);
+    __type(value, struct wl_val);
+} watchlist SEC(".maps");
+
+static __always_inline int is_tracked(pid_t pid)
+{
+    return bpf_map_lookup_elem(&tracked, &pid) != NULL;
+}
 
 SEC("tp/syscalls/sys_enter_execve")
-int handle_execve(struct trace_event_raw_sys_enter *ctx){
+int handle_execve(struct trace_event_raw_sys_enter *ctx)
+{
     pid_t pid = bpf_get_current_pid_tgid() >> 32;
-    if(!bpf_map_lookup_elem(&tracked,&pid)){
+    if (!is_tracked(pid))
         return 0;
-    }
-    struct event *e = bpf_ringbuf_reserve(&events,sizeof(*e),0);
-    if(!e) return 0;
 
+    struct event *e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
+    if (!e)
+        return 0;
+
+    e->type = EVENT_EXEC;
     e->pid = pid;
     e->uid = bpf_get_current_uid_gid() & 0xffffffff;
-    bpf_get_current_comm(&e->comm,sizeof(e->comm));
-    bpf_probe_read_user_str(&e->filename,sizeof(e->filename),(const char*)ctx->args[0]);
-    bpf_ringbuf_submit(e,0);
+    bpf_get_current_comm(&e->comm, sizeof(e->comm));
+    bpf_probe_read_user_str(&e->exec.filename, sizeof(e->exec.filename),
+                            (const char *)ctx->args[0]);
+    bpf_ringbuf_submit(e, 0);
     return 0;
 }
 
-SEC("tp/syscalls/sys_enter_openat")
-int handle_openat(struct trace_event_raw_sys_enter *ctx){
-    char path[64];
-    bpf_probe_read_user_str(&path,sizeof(path),(const char*)ctx->args[1]);
-    char target[] = "/etc/passwd";
-    for(int i = 0;i<sizeof(target);i++){
-        if(path[i]!=target[i]){
-            return 0;
-        }
-    }
-    
-    struct event *e = bpf_ringbuf_reserve(&events,sizeof(*e),0);
-    if(!e) return 0;
-    e->pid = bpf_get_current_pid_tgid() >> 32;
-    e->uid =  bpf_get_current_uid_gid() & 0xffffffff;
-    bpf_get_current_comm(&e->comm,sizeof(e->comm));
-    bpf_probe_read_user_str(&e->filename,sizeof(e->filename),(const char*)ctx->args[1]);
+SEC("kprobe/security_file_open")
+int BPF_KPROBE(handle_file_open, struct file *file)
+{
+    pid_t pid = bpf_get_current_pid_tgid() >> 32;
+    if (!is_tracked(pid))
+        return 0;
+
+    struct file_key key = {};
+    key.ino = BPF_CORE_READ(file, f_inode, i_ino);
+
+    struct wl_val *hit = bpf_map_lookup_elem(&watchlist, &key);
+    if (!hit)
+        return 0;
+
+    struct event *e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
+    if (!e)
+        return 0;
+
+    e->type = EVENT_FILE_OPEN;
+    e->pid = pid;
+    e->uid = bpf_get_current_uid_gid() & 0xffffffff;
+    bpf_get_current_comm(&e->comm, sizeof(e->comm));
+    __builtin_memcpy(&e->file.path, hit->path, sizeof(e->file.path));
+    bpf_ringbuf_submit(e, 0);
     return 0;
 }
