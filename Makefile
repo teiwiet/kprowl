3eeb78a88c8b2c0dd6cef7d425f20864504ccaf0
teiwiet <teiwiet33@gmail.com>
2026-07-30 19:31:12 +0700

    feat : add watchlist and another struct to handle security_file_open

--- Makefile
+++ Makefile
@@ -5,7 +5,11 @@ all: kprowl.bpf.o loader
 
 kprowl.bpf.o: kprowl.bpf.c
 	$(CLANG) $(FLAGS) -c $< -o $@
+
 loader: loader.c
 	$(CC) $< -o $@ -lbpf
+
 clean:
 	rm -f kprowl.bpf.o loader 
+
+.PHONY: all clean
--- kprowl.h
+++ kprowl.h
@@ -1,12 +1,35 @@
 #ifndef __KPROWL_H
 #define __KPROWL_H
-#define TASK_COMM_LEN 16
+
+#define TASK_COMM_LEN    16
 #define MAX_FILENAME_LEN 256
-struct event{
+
+enum event_type {
+    EVENT_EXEC = 0,
+    EVENT_FILE_OPEN,
+};
+
+struct file_key {
+    __u64 ino;
+};
+
+struct wl_val {
+    char path[MAX_FILENAME_LEN];
+};
+
+struct event {
+    __u32 type;
     __u32 pid;
     __u32 uid;
-    char comm[TASK_COMM_LEN];
-    char filename[MAX_FILENAME_LEN];
+    char  comm[TASK_COMM_LEN];
+    union {
+        struct {
+            char filename[MAX_FILENAME_LEN];
+        } exec;
+        struct {
+            char path[MAX_FILENAME_LEN];
+        } file;
+    };
 };
 
 #endif
