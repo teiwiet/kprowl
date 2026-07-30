4dc8651811cbb9f57bfd163901accbeab2de99e4
teiwiet <teiwiet33@gmail.com>
2026-07-30 23:40:36 +0700

    feat: add security_socket_connect kprobe

--- kprowl.bpf.c
+++ kprowl.bpf.c
@@ -4,6 +4,8 @@
 #include <bpf/bpf_helpers.h>
 #include "kprowl.h"
 
+#define AF_INET 2
+
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 struct {
@@ -74,6 +76,34 @@ int BPF_KPROBE(handle_file_open, struct file *file)
     e->uid = bpf_get_current_uid_gid() & 0xffffffff;
     bpf_get_current_comm(&e->comm, sizeof(e->comm));
     __builtin_memcpy(&e->file.path, hit->path, sizeof(e->file.path));
+    bpf_ringbuf_submit(e, 0);
+    return 0;
+}
+
+SEC("kprobe/security_socket_connect")
+int BPF_KPROBE(handle_connect, struct socket *sock, struct sockaddr *address, int addrlen)
+{
+    pid_t pid = bpf_get_current_pid_tgid() >> 32;
+    if (!is_tracked(pid))
+        return 0;
+
+    __u16 family = BPF_CORE_READ(address, sa_family);
+    if (family != AF_INET)
+        return 0;
+
+    struct sockaddr_in *in = (struct sockaddr_in *)address;
+
+    struct event *e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
+    if (!e)
+        return 0;
+
+    e->type = EVENT_CONNECT;
+    e->pid = pid;
+    e->uid = bpf_get_current_uid_gid() & 0xffffffff;
+    bpf_get_current_comm(&e->comm, sizeof(e->comm));
+    e->net.family = family;
+    e->net.daddr = BPF_CORE_READ(in, sin_addr.s_addr);
+    e->net.dport = BPF_CORE_READ(in, sin_port);
     bpf_ringbuf_submit(e, 0);
     return 0;
 }
